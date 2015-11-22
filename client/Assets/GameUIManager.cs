a879a6313fcb58072af33542acda6bf7110bb561
wuyinjie <1518986465@qq.com>
2015-11-22 23:26:50 +0800

    client：客户端UI部署

---
+++ client/Assets/GameUIManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class GameUIManager : MonoBehaviour
+{
+
+    public void OnStartGame()
+    {
+        Debug.Log("Start Game!!");
+    }
+
+    public void OnOpenSettingDialog()
+    {
+        Debug.Log("Open Settting Dialog!!");
+    }
+}
