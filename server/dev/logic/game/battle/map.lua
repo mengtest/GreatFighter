75b1ba1295de9731944d4754739b94c131d5f83d
wuyinjie <1518986465@qq.com>
2016-04-09 20:41:02 +0800

    server：添加场景和地图类接口

---
+++ server/dev/logic/game/battle/map.lua
@@ -0,0 +1,22 @@
+--[[
+	desc:地图类
+	author:wuyinjie
+	since:2016-4-9
+]]
+
+local class = require "dev.core.class"
+local map = class()
+
+function map:ctor()
+end
+
+function map:init()
+end
+
+function map:fixPosition()
+end
+
+function map:findRoutePath()
+end
+
+return map
---
+++ server/dev/logic/game/battle/scene.lua
@@ -0,0 +1,46 @@
+--[[
+	desc:场景类
+	author:wuyinjie
+	since:2016-4-9
+]]
+
+local class = require "dev.core.class"
+local scene = class()
+
+function scene:ctor()
+end
+
+function scene:init()
+end
+
+function scene:skill()
+end
+
+function scene:move()
+end
+
+function scene:calcAoiMsg()
+end
+
+function scene:calcPushMsg()
+end
+
+function scene:reset()
+end
+
+function scene:update()
+end
+
+function scene:checkGameStatus()
+end
+
+function scene:onGameOver()
+end
+
+function scene:onEnter()
+end
+
+function scene:onExit()
+end
+
+return scene
