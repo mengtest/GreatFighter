a3cd3d5816e37254d4449807f459a240ef61ffac
wuyinjie <1518986465@qq.com>
2017-01-24 00:25:46 +0800

    client：完成PC工程部署

--- client/Trunk/Src/Core/AppDelegate.cpp
+++ client/Trunk/Src/Core/AppDelegate.cpp
@@ -39,7 +39,10 @@ bool AppDelegate::applicationDidFinishLaunching() {
     // set FPS. the default value is 1.0/60 if you don't call this
     director->setAnimationInterval(1.0 / 60);
 
-    FileUtils::getInstance()->addSearchPath("res");
+    FileUtils::getInstance()->addSearchPath("Art/Effect");
+	FileUtils::getInstance()->addSearchPath("Art/Fonts");
+	FileUtils::getInstance()->addSearchPath("Art/Scene");
+	FileUtils::getInstance()->addSearchPath("Art/UI");
 
     // create a scene. it's an autorelease object
     auto scene = HelloWorld::createScene();
--- client/Trunk/Src/main.cpp
+++ client/Trunk/Src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "Core\AppDelegate.h"
 using namespace std;
 
 void main()
 {
-	cout << "HelloWorld" << endl;
+	AppDelegate app;
+	cocos2d::Application::getInstance()->run();
 }
