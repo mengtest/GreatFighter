b5485c564f8d1260d2a065eb5d910215871d6224
wuyinjie <1518986465@qq.com>
2017-02-14 20:27:12 +0800

    client：添加常量定义文件

---
+++ client/Trunk/Src/Common/IGConst.h
@@ -0,0 +1,14 @@
+// desc: 常量定义
+// author : wuyinjie
+// since : 2017-02-13
+#pragma once
+
+enum class IGClientProtoType
+{
+	DEFAULT = 0,
+};
+
+enum class IGServerNotifyType
+{
+	DEFAULT = 0,
+};
