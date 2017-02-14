670aeb93732af6a4583872b5f0a120f688f6e309
wuyinjie <1518986465@qq.com>
2017-02-15 01:32:52 +0800

    server：合并脚本

--- server/shell/build.sh
+++ server/shell/build.sh
@@ -1,42 +1,86 @@
 #!/bin/bash
 
 # /***
-# * GreatFighter编译脚本
+# * GreatFighter编译、清理脚本
 # * @author wuyinjie
 # * @since 2017-01-11
 # */
 
 parentPath=$(dirname $(pwd))
 
-# 编译skynet
-echo "====================="
-echo "start build skyent..."
-cd $parentPath/skynet
-make linux MALLOC_STATICLIB= SKYNET_DEFINES=-DNOUSE_JEMALLOC
+function build() {
+	# 编译skynet
+	echo "====================="
+	echo "start build skyent..."
+	cd $parentPath/skynet
+	make linux MALLOC_STATICLIB= SKYNET_DEFINES=-DNOUSE_JEMALLOC
 
-# 编译log日志服务
-echo "====================="
-echo "start build service log..."
-cd $parentPath/3rd/service-log
-make
+	# 编译log日志服务
+	echo "====================="
+	echo "start build service log..."
+	cd $parentPath/3rd/service-log
+	make
 
-# 编译telnet服务
-echo "====================="
-echo "start build service telnet..."
-cd $parentPath/3rd/service-telnetd
-make
+	# 编译telnet服务
+	echo "====================="
+	echo "start build service telnet..."
+	cd $parentPath/3rd/service-telnetd
+	make
 
-# 编译cjson库
-echo "====================="
-echo "start build lua-cjson..."
-cd $parentPath/3rd/lua-cjson
-make
+	# 编译cjson库
+	echo "====================="
+	echo "start build lua-cjson..."
+	cd $parentPath/3rd/lua-cjson
+	make
 
-# 编译redis
-echo "====================="
-echo "start unpack redis..."
-cd $parentPath/3rd
-tar -zxvf redis-3.2.8.tar.gz
-echo "start build redis..."
-cd $parentPath/3rd/redis-3.2.8
-make
+	# 编译redis
+	echo "====================="
+	echo "start unpack redis..."
+	cd $parentPath/3rd
+	tar -zxvf redis-3.2.8.tar.gz
+	echo "start build redis..."
+	cd $parentPath/3rd/redis-3.2.8
+	make
+}
+
+function clean() {
+	# 清理skynet
+	echo "====================="
+	echo "start clean skyent..."
+	cd $parentPath/skynet
+	make clean
+
+	# 清理log日志服务
+	echo "====================="
+	echo "start clean service log..."
+	cd $parentPath/3rd/service-log
+	make clean
+
+	# 清理telnet服务
+	echo "====================="
+	echo "start clean service telnet..."
+	cd $parentPath/3rd/service-telnetd
+	make clean
+
+	# 清理cjson库
+	echo "====================="
+	echo "start clean cjson..."
+	cd $parentPath/3rd/lua-cjson
+	make clean
+
+	# 清理redis
+	echo "====================="
+	echo "start clean redis..."
+	cd $parentPath/3rd/redis-3.2.8
+	make clean
+	cd $parentPath/3rd
+	rm -rf redis-3.2.8
+}
+
+if [[ "$1" == "all" ]]; then
+	build
+elif [[ "$1" == "clean" ]]; then
+	clean
+else
+	echo "不存在$1指令"
+fi
---
+++ server/shell/service.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# /***
+# * GreatFighter服务端启动关闭脚本
+# * @author wuyinjie
+# * @since 2017-01-11
+# */
+
+# 启动脚本
+parentPath=$(dirname $(pwd))
+
+function start_process() {
+	ps -o command -C skynet | grep "$parentPath/skynet/skynet $parentPath/dev/$1/conf$2" &> /dev/null
+	[ $? -eq 0 ] && echo "进程$1已经存在,禁止重复启动" && return
+	echo "$parentPath/skynet/skynet $parentPath/dev/$1/conf$2 &> /dev/null &"
+	nohup $parentPath/skynet/skynet $parentPath/dev/$1/conf$2 &> /dev/null &
+}
+
+function launch_all() {
+	start_process gamecenter
+	sleep 10
+
+	start_process loginsvr
+	start_process gm
+
+	start_process gamesvr
+	start_process battle
+	start_process verify
+}
+
+function stop_process() {
+	echo "killing $1 ..."
+	res=`ps aux | grep "$1" | grep -v tail | grep -v grep | awk '{print $2}'`
+	[ "$res" != "" ] && kill $res
+}
+
+function stop_all() {
+	stop_process loginsvr
+	stop_process gm
+
+	stop_process gamesvr
+	stop_process battle
+	stop_process verify
+
+	stop_process gamecenter
+}
+
+if [[ "$1" == "start" ]]; then
+	launch_all()
+elif [[ "$1" == "stop" ]]; then
+	stop_all()
+else
+	echo "不存在$1指令"
+fi
